956018f889b12eadc9466068ac19588e9ea2549b
Winston <euphonic.disorder@gmail.com>
2022-06-13 11:05:31 -0400

    better work

---
+++ node_modules/.cache/nuxt/dist/server/pages/resources.js
@@ -0,0 +1,65 @@
+exports.ids = [6];
+exports.modules = {
+
+/***/ 26:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/resources.vue?vue&type=template&id=1343fc1b&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"space-y-8 min-h-screen p-8"},[_vm._ssrNode("<h1 class=\"text-black flex text-black font-bold font-mono text-5xl\">\n        Resources\n        \n    </h1> <h2 class=\"text-3xl font-semibold\">Lockheed Martin Presentation</h2> <iframe src=\"https://docs.google.com/presentation/d/e/2PACX-1vSjRPz_224kbdqWM1F1JNc6crgLJKIJ57FbShK7L9JyJXyjurd_f8Oot2XvtL8OQw/embed?start=false&loop=false&delayms=3000\" frameborder=\"0\" width=\"1280\" height=\"749\" allowfullscreen=\"allowfullscreen\" mozallowfullscreen=\"true\" webkitallowfullscreen=\"true\"></iframe> <h2 class=\"text-3xl font-semibold\">Challenge Night Presentation</h2> <iframe src=\"https://docs.google.com/presentation/d/e/2PACX-1vQy0bFFVBUAuDdi2OO21IJ6F9MhWlgQk9d-DPRUDjOOr75hnzrDYuZPWRpb4ZNY8iUgZM0UsIc3A7Cv/embed?start=false&loop=false&delayms=3000\" frameborder=\"0\" width=\"960\" height=\"569\" allowfullscreen=\"allowfullscreen\" mozallowfullscreen=\"true\" webkitallowfullscreen=\"true\"></iframe>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./pages/resources.vue?vue&type=template&id=1343fc1b&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/resources.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var resourcesvue_type_script_lang_js_ = ({});
+// CONCATENATED MODULE: ./pages/resources.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pages_resourcesvue_type_script_lang_js_ = (resourcesvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/resources.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_resourcesvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "52ed4922"
+  
+)
+
+/* harmony default export */ var resources = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=resources.js.map
