5e1061e3cf215c01506a2b8be695c9029706b986
Winston <euphonic.disorder@gmail.com>
2022-06-13 10:55:31 -0400

    fixed

---
+++ pages/about.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="space-y-8 min-h-screen p-8">
+      <h1 class="text-black flex text-black font-bold font-mono text-5xl">
+          About
+      </h1>
+      <div class="space-y-2">
+          <h2 class="text-3xl font-semibold">Colin</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus sem metus, nec tempor nulla molestie id. Donec hendrerit scelerisque felis nec feugiat. Nulla id efficitur ligula. Aliquam sed dictum sem. Nam volutpat turpis varius nisl cursus, quis varius metus tincidunt. Aenean et ante libero. Suspendisse sagittis lectus a ligula scelerisque euismod. Suspendisse eget aliquet dui. Vivamus ultrices vulputate iaculis. Aliquam eget aliquam neque. Vestibulum in fringilla magna, et iaculis risus. Aenean aliquet ut nibh quis sollicitudin. Mauris vestibulum eros vel erat sodales ullamcorper. Sed euismod at tortor et tincidunt. </p>
+      </div>
+
+    <div class="space-y-2">
+          <h2 class="text-3xl font-semibold">Caleb</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus sem metus, nec tempor nulla molestie id. Donec hendrerit scelerisque felis nec feugiat. Nulla id efficitur ligula. Aliquam sed dictum sem. Nam volutpat turpis varius nisl cursus, quis varius metus tincidunt. Aenean et ante libero. Suspendisse sagittis lectus a ligula scelerisque euismod. Suspendisse eget aliquet dui. Vivamus ultrices vulputate iaculis. Aliquam eget aliquam neque. Vestibulum in fringilla magna, et iaculis risus. Aenean aliquet ut nibh quis sollicitudin. Mauris vestibulum eros vel erat sodales ullamcorper. Sed euismod at tortor et tincidunt. </p>
+      </div>
+
+    <div class="space-y-2">
+          <h2 class="text-3xl font-semibold">Jai</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus sem metus, nec tempor nulla molestie id. Donec hendrerit scelerisque felis nec feugiat. Nulla id efficitur ligula. Aliquam sed dictum sem. Nam volutpat turpis varius nisl cursus, quis varius metus tincidunt. Aenean et ante libero. Suspendisse sagittis lectus a ligula scelerisque euismod. Suspendisse eget aliquet dui. Vivamus ultrices vulputate iaculis. Aliquam eget aliquam neque. Vestibulum in fringilla magna, et iaculis risus. Aenean aliquet ut nibh quis sollicitudin. Mauris vestibulum eros vel erat sodales ullamcorper. Sed euismod at tortor et tincidunt. </p>
+      </div>
+      <div class="space-y-2">
+          <h2 class="text-3xl font-semibold">Advisor: Phil</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus sem metus, nec tempor nulla molestie id. Donec hendrerit scelerisque felis nec feugiat. Nulla id efficitur ligula. Aliquam sed dictum sem. Nam volutpat turpis varius nisl cursus, quis varius metus tincidunt. Aenean et ante libero. Suspendisse sagittis lectus a ligula scelerisque euismod. Suspendisse eget aliquet dui. Vivamus ultrices vulputate iaculis. Aliquam eget aliquam neque. Vestibulum in fringilla magna, et iaculis risus. Aenean aliquet ut nibh quis sollicitudin. Mauris vestibulum eros vel erat sodales ullamcorper. Sed euismod at tortor et tincidunt. </p>
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <div class="text-black items-center ">
+          <div class="border-8 border-black py-48 px-16 m-16 space-y-4">
+              <div class="font-bold text-5xl lg:text-8xl text-center whitespace-pre-line">The 5Ds and you<span class="">.</span> </div>
+              <div class="font-mono text-3xl text-center whitespace-pre-line">millions of jobs, billions of dollars, everyone's lives</div>
+          </div>
+        <div class="text-gray-400 italic text-center text-2xl mt-32">
+            Presented by:
+        </div>
+              <div class="md:flex justify-center">
+                  <img class="w-2/3 md:w-1/2 p-12 " src="/LM-logo.png"/>
+                  <img class="w-1/2 md:w-1/4 p-12" src="/2021 STEM logo.png"/>
+              </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
